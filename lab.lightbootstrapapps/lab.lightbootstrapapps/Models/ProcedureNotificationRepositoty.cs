17cad8d3cc2bcd5d8937fc78780d490f8c13287b
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2017-08-03 15:17:31 +0600

    first commit

---
+++ lab.lightbootstrapapps/lab.lightbootstrapapps/Models/ProcedureNotificationRepositoty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab.lightbootstrapapps.Models
+{
+    public class ProcedureNotificationRepositoty
+    {
+        private PlacovuOntrackHealth_DevEntities _context;
+
+        public ProcedureNotificationRepositoty()
+        {
+            _context = new PlacovuOntrackHealth_DevEntities();
+        }
+
+
+    }
+}
