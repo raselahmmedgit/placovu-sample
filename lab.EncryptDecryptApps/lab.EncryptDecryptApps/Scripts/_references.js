72679de1988f6917176f02cc4b84aaa4693d6497
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2017-12-28 17:44:56 +0600

    2017-12-28

--- lab.EncryptDecryptApps/lab.EncryptDecryptApps/Scripts/_references.js
+++ lab.EncryptDecryptApps/lab.EncryptDecryptApps/Scripts/_references.js
@@ -1,5 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
+/// <reference path="jquery.mask.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="jquery-1.12.4.js" />
