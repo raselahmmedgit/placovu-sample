0e5da6f6107b4979b30c4500fcf4c3f8205b600c
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2018-01-22 09:52:34 +0600

    2018-01-22

---
+++ rabapp/rabapp.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rabapp.web.Startup))]
+namespace rabapp.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
