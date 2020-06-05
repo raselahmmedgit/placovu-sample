37d49fce17825f76800667833ebc454b92a336be
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2020-06-05 17:47:40 +0600

    2020-06-05

---
+++ RnD.SocialMediaAuth/RnD.SocialMediaAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RnD.SocialMediaAuth.MvcApplication" Language="C#" %>
---
+++ RnD.SocialMediaAuth/RnD.SocialMediaAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RnD.SocialMediaAuth.Startup))]
+namespace RnD.SocialMediaAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
